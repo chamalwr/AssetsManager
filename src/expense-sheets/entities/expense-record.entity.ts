d71fe1146bf578b4d79d37e83328fd6e1fbe83c2
Chamal Weerasinghe <chamalwr@users.noreply.github.com>
2021-12-29 23:55:26 +0530

    Added expense record id field

--- src/expense-sheets/entities/expense-record.entity.ts
+++ src/expense-sheets/entities/expense-record.entity.ts
@@ -1,10 +1,13 @@
-import { Field, ObjectType } from "@nestjs/graphql";
+import { Field, ID, ObjectType } from "@nestjs/graphql";
 import { Prop, Schema } from "@nestjs/mongoose";
 import { ExpenseCategory } from "src/expense-categories/entities/expense-category.entity";
 
 @ObjectType()
 @Schema()
 export class ExpenseRecord {
+    @Field(() => ID, { description: 'Expense Record ID' })
+    _id: string;
+
     @Field(() => ExpenseCategory, { description : 'Expense Category' })
     @Prop()
     expenseCategory: ExpenseCategory;
